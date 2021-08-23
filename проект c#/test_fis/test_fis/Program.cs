56ed7d7f14084b5d0da1f1408965faad42e4eb0b
SmolyakovFIT12 <dfa85kt65@mail.ru>
2021-08-23 12:11:57 +0500

    commit

---
+++ проект c#/test_fis/test_fis/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace test_fis
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            SQLConnect connect = new SQLConnect();
+            try
+            {
+                connect.connectOpen();// открываем соединение 
+                var cmd = connect.getAngryReader();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())// читаем результат запроса
+                    {
+                        string name = (string)reader["name"];
+                        Console.WriteLine("Самый злобный читатель: " + name);
+                    }
+                }
+                connect.connectClose();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            
+        }
+    }
+}
---
+++ проект c#/test_fis/test_fis/SQLConnect.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace test_fis
+{
+    public class SQLConnect
+    {
+        SqlConnection connect;
+        public SQLConnect()
+        {
+            var sConnStr = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"LAPTOP-1GAL0TI1\SQLEXPRESS",// имя сервера
+                InitialCatalog = "test_fis",// название бд
+                IntegratedSecurity = true
+            }.ConnectionString;
+            connect = new SqlConnection(sConnStr);
+        }
+
+
+
+        public SqlCommand getAngryReader()//получить имя злобного читателя
+        {
+
+            var sCommand = new SqlCommand()
+            {
+                Connection = connect,
+                CommandText = @"SELECT top 1 name FROM student inner join Orders on student.id = Orders.id_student
+group by name
+order by count(Orders.id) desc"
+//текст запроса в базе данных
+            };
+
+            return sCommand;
+        }
+
+
+        public void connectOpen()
+        {
+            connect.Open();
+        }
+
+        public void connectClose()
+        {
+            connect.Close();
+        }
+
+
+    }
+}
